b11495033f85a1e48bc68c3293a5c16afa2c52d5
goseungjo <rhtmdwh@naver.com>
2023-01-10 15:10:48 +0900

    깃 올리기

---
+++ src/main/java/study/board/IndexController.java
@@ -0,0 +1,33 @@
+package study.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/header")
+    public String header(){
+        return "/header";
+    }
+
+    @GetMapping("/main")
+    public String main(){
+        return "/main";
+    }
+
+    @GetMapping("/sidebar")
+    public String sidebar(){
+        return "/sidebar";
+    }
+
+    @GetMapping("/write")
+    public String write(){
+        return "/write";
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+        return "/detail";
+    }
+}
